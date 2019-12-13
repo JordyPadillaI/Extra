6ca209285e3d00d6ed7472342ea34c115c318697
JordyPadillaI <jordy.padilla@alumnos.udg.mx>
2019-12-13 04:36:20 -0600

    Cambios

--- blog/resources/views/landing-page.blade.php
+++ blog/resources/views/landing-page.blade.php
@@ -1,17 +1,38 @@
 <!doctype html>
 <html>
 <head>
-<title>Asistencia</title>
+<title>Iniciar Sesión</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
 <!--css-->
 </head>
 <body>
-<div>
-HEADER
+<div class="col-md-6">
+<div class="alert alert-success" role="alert">
+  <h4 class="alert-heading">Iniciar Sesión</h4>
+ <hr>
+<form>
+<div class="container">
+<div class="col-md-7">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Correo Electrónico</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <small id="emailHelp" class="form-text text-muted">No usaremos tu correo para otros fines.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Contraseña</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Recordar</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Iniciar</button>
+  </div>
+  </div>
+</form>
 </div>
-<div>
-NAV BAR / MENU
 </div>
-<h1>HOLA MUNDO DESDE VISTA </h1>
 
 </body>
 </html>
--- blog/resources/views/registro-page.blade.php
+++ blog/resources/views/registro-page.blade.php
@@ -2,30 +2,65 @@
 <html>
 <head>
 <title>Registro</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
 <!--css-->
 </head>
 <body>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Contacto</div>
-
-                <div class="card-body">
-                <form>
-  <div class="form-group">
-    <label for="correo">Email address</label>
-    <input type="email" name="correo" class="form-control" id="correo" placeholder="name@example.com">
+<div class="container-fluid" >
+<div class="col-sm" >
+<div class="card">
+  <div class="card-header">
+    REGISTRO
   </div>
-  <div class="form-group">
-    <label for="comentario">Example textarea</label>
-    <textarea name="comentario" class="form-control" id="comentario" rows="3"></textarea>
-  </div>
-</form>
-                </div>
-            </div>
-        </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+    <form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Correo Electrónico</label>
+      <input type="email" class="form-control" id="inputEmail4">
     </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Contraseña</label>
+      <input type="password" class="form-control" id="inputPassword4">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Domicilio</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="Ingrese calle y número...">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-5">
+      <label for="inputCity">Ciudad</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="form-row">
+    <div class="form-group col-md-10">
+      <label for="inputState">Estado</label>
+      <input type="text" class="form-control" id="inputState">
+    </div>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Zip</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Recordar
+      </label>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Registrase</button>
+</form>
+      
+    </blockquote>
+  </div>
+</div>
 </div>
 </body>
 </html>
